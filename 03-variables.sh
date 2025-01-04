e5641f1937dc0823cf3163fbe5996b70e45ca02c
vinod vannada <vinodgoud1518@gmail.com>
2025-01-03 21:45:32 -0500

    conversation added

---
+++ 03-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+PERSON1=Rahim
+PERSON2=Robert
+
+
+
+echo "Rahim:: Hi Robert"
+echo "Robert:: Hello Rahim"
+echo "Rahim:: How are you doing?"
+echo "Robert:: I am good. How are you?"
