4349399ab837212eeac220169c68d5b01e74b70b
vinod vannada <vinodgoud1518@gmail.com>
2025-01-03 22:15:18 -0500

    conversation added

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Please enter your username::" #text entered here will be added as a value to variable
+
+read -s USERNAME # Here USERNAME is a variable name
+
+echo "Please enter your password::"
+
+read -s
