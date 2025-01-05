4247707ed6f7c640b089ba0c17a273fc8d1c5dea
vinod vannada <vinodgoud1518@gmail.com>
2025-01-04 22:48:42 -0500

    shell script

---
+++ 09-condition.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+NUMBER=$1
+
+# -gt, -lt, -ge, -le
+if [ $NUMBER -gt 100 ]
+then
+    echo "Given number is greater than 100"
+else
+    echo "Given mumber is less then or equal to 100"
+fi        
